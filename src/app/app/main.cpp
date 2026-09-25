2f9aa6db3f63df807b19e2bc1073359cf3827221
DESKTOP-PQC9BC1\IlyaS <ivsosnin01@gmail.com>
2026-09-25 22:52:26 +0700

    В main добавлен вывод для проверки

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -1,35 +1,88 @@
 #include "DiffieHellman/DiffieHellman.h"
+#include "DiffieHellmanSubgroup/DiffieHellmanSubgroup.h"
+
 #include <iostream>
 
-int main() {
-
+void multiplicativeGroupDH() {
+    std::cout << "Multiplicative group\n";
     DiffieHellman alice;
 
-    alice.generateParams();
+ alice.generateParams();
+
+    std::cout << "Alice params: \n";
     alice.printParams();
 
     alice.generateKeys();
+    std::cout << "Alice keys: \n";
     alice.printKeys();
 
-    DiffieHellman bob(alice.getParams());
+    DiffieHellmanSubgroup bob(alice.getParams());
+    std::cout << "Bob params: \n";
     bob.printParams();
 
     bob.generateKeys();
+    std::cout << "Bob keys: \n";
     bob.printKeys();
 
     bob.setPeerPublicKey(alice.getPublicKey());
-    alice.printPeerPublicKey();
-
     alice.setPeerPublicKey(bob.getPublicKey());
+
+    std::cout << "Alice peer public key: \n";
     alice.printPeerPublicKey();
-
-    alice.calculateSharedSecret();
-
-    bob.calculateSharedSecret();
+    std::cout << "Bob peer public key: \n";
     bob.printPeerPublicKey();
 
+    alice.calculateSharedSecret();
+    bob.calculateSharedSecret();
+
+    std::cout << "Alice share secret: \n";
     alice.printSharedSecret();
+    std::cout << "Bob share secret: \n";
     bob.printSharedSecret();
+}
+
+void cyclicGroupDH() {
+    std::cout << "Cyclic group\n";
+    DiffieHellmanSubgroup alice;
+
+    alice.generateParams();
+
+    std::cout << "Alice params: \n";
+    alice.printParams();
+
+    alice.generateKeys();
+    std::cout << "Alice keys: \n";
+    alice.printKeys();
+
+    DiffieHellmanSubgroup bob(alice.getParams());
+    std::cout << "Bob params: \n";
+    bob.printParams();
+
+    bob.generateKeys();
+    std::cout << "Bob keys: \n";
+    bob.printKeys();
+
+    bob.setPeerPublicKey(alice.getPublicKey());
+    alice.setPeerPublicKey(bob.getPublicKey());
+
+    std::cout << "Alice peer public key: \n";
+    alice.printPeerPublicKey();
+    std::cout << "Bob peer public key: \n";
+    bob.printPeerPublicKey();
+
+    alice.calculateSharedSecret();
+    bob.calculateSharedSecret();
+
+    std::cout << "Alice share secret: \n";
+    alice.printSharedSecret();
+    std::cout << "Bob share secret: \n";
+    bob.printSharedSecret();
+}
+
+int main() {
+    multiplicativeGroupDH();
+    std::cout << "\n-------------------------------------\n\n";
+    cyclicGroupDH();
 
     return 0;
 } 
